8277e8642069dc6e4c4e428c8e79890620ed9a03
Thomas Jensen <thomas.jensen_@outlook.com>
2024-04-23 21:25:25 +0200

    Added a new solution

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -2,17 +2,31 @@ const answer = []
 
 // Write your code below this line
 
-for (i = 1; i <= 15; i++) {
-  if (i === 3 || i === 6 || i === 9 || i === 12) {
-    answer.push('Fizz')
-  } else if (i === 5 || i === 10) {
-    answer.push('Buzz')
-  } else if (i === 15) {
-    answer.push('FizzBuzz')
-  } else {
-    answer.push(i)
+// for (i = 1; i <= 15; i++) {
+//   if (i === 3 || i === 6 || i === 9 || i === 12) {
+//     answer.push('Fizz')
+//   } else if (i === 5 || i === 10) {
+//     answer.push('Buzz')
+//   } else if (i === 15) {
+//     answer.push('FizzBuzz')
+//   } else {
+//     answer.push(i)
+//   }
+//   console.log(answer)
+// }
+
+// Improved the loop to work regardless of length
+const iterations = 15
+
+for (i = 1; i <= Number(iterations); i++) {
+  let fizzbuzz = ''
+  if (i % 3 === 0) {
+    fizzbuzz += 'Fizz'
   }
-  console.log(answer)
+  if (i % 5 === 0) {
+    fizzbuzz += 'Buzz'
+  }
+  answer.push(fizzbuzz || i)
 }
 
 // Don't touch the code below this line, we'll cover it later
